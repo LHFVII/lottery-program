629f62231def648098c0ba71a363d925096bd009
LHF009 <rodriguezpatricio567@gmail.com>
2024-12-31 18:08:42 -0300

    added new instruction

--- programs/lottery/src/instructions/mod.rs
+++ programs/lottery/src/instructions/mod.rs
@@ -2,8 +2,10 @@ pub mod buy_ticket;
 pub mod choose_winner;
 pub mod commit_winner;
 pub mod initialize;
+pub mod initialize_lottery;
 
 pub use buy_ticket::*;
 pub use choose_winner::*;
 pub use commit_winner::*;
 pub use initialize::*;
+pub use initialize_lottery::*;
