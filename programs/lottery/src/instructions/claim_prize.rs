f6620cd5417ae38a9fa6e73b937754fbea8d6295
LHF009 <rodriguezpatricio567@gmail.com>
2025-01-02 23:08:08 -0300

    Added account instruction

--- programs/lottery/src/instructions/claim_prize.rs
+++ programs/lottery/src/instructions/claim_prize.rs
@@ -1,4 +1,7 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{token::Mint, token_interface::TokenInterface};
+
+use super::TokenLottery;
 
 pub fn claim_prize(ctx: Context<ClaimPrize>) -> Result<()> {
     Ok(())
@@ -6,5 +9,18 @@ pub fn claim_prize(ctx: Context<ClaimPrize>) -> Result<()> {
 
 #[derive(Accounts)]
 pub struct ClaimPrize<'info> {
+    pub signer: Signer<'info>,
+    #[account(
+        mut,
+        seeds=[b"token_lottery".as_ref()],
+        bump = token_lottery.bump
+    )]
+    pub token_lottery: Account<'info, TokenLottery>,
+    #[account(
+        seeds=[token_lottery.winner.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub ticket_mint: InterfaceAccount<'info, Mint>,
+    pub token_program: Interface<'info, TokenInterface>,
     pub system_program: Program<'info, System>,
 }
