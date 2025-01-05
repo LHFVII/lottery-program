a3a7aa259f3f8f0d5db325e680201d51bcb98764
LHF009 <rodriguezpatricio567@gmail.com>
2025-01-04 21:39:41 -0300

    added more fields

--- programs/lottery/src/instructions/claim_prize.rs
+++ programs/lottery/src/instructions/claim_prize.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_spl::{token::Mint, token_interface::TokenInterface};
+use anchor_spl::{metadata::{Metadata, MetadataAccount}, token::{Mint, TokenAccount}, token_interface::TokenInterface};
 
 use super::TokenLottery;
 
@@ -21,6 +21,37 @@ pub struct ClaimPrize<'info> {
         bump,
     )]
     pub ticket_mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        seeds = [b"collection_mint".as_ref()],
+        bump
+    )]
+    pub collection_mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        seeds= [b"metadata", token_metadata_program.key().as_ref(), ticket_mint.key().as_ref()],
+        bump,
+        seeds::program = token_metadata_program.key()
+    )]
+    pub ticket_metadata: Account<'info, MetadataAccount>,
+
+    #[account(
+        associated_token::mint = ticket_mint,
+        associated_token::authority = signer,
+        associated_token::token_program = token_program,
+    )]
+    pub ticket_account: InterfaceAccount<'info,TokenAccount>,
+
+    #[account(
+        seeds= [b"metadata", token_metadata_program.key().as_ref(), collection_mint.key().as_ref()],
+        bump,
+        seeds::program = token_metadata_program.key()
+    )]
+    pub collection_metadata: Account<'info, MetadataAccount>,
+
+
+
+    pub token_metadata_program: Program<'info, Metadata>,
     pub token_program: Interface<'info, TokenInterface>,
     pub system_program: Program<'info, System>,
 }
